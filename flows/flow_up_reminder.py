f054a10b2ee81cc867fa2ca822bcf83c93346f11
Robert Vo <robertvo.developer.93@gmail.com>
2025-03-11 12:46:44 +0700

    add logger timing

--- flows/flow_up_reminder.py
+++ flows/flow_up_reminder.py
@@ -1,34 +1,34 @@
 import asyncio
 from datetime import datetime
-from typing import Any
+# from typing import Any
 from prefect import flow, get_run_logger, tags, task
-from pydantic import BaseModel
-from prefect_sqlalchemy import DatabaseCredentials
-from prefect_sqlalchemy.database import sqlalchemy_query
+# from pydantic import BaseModel
+# from prefect_sqlalchemy import DatabaseCredentials
+# from prefect_sqlalchemy.database import sqlalchemy_query
 
-sqlalchemy_credentials = DatabaseCredentials.load("postgres-db")
+# sqlalchemy_credentials = DatabaseCredentials.load("postgres-db")
 
 
-class Activity(BaseModel):
-    id: int
-    name: str
-    created_at: datetime
+# class Activity(BaseModel):
+#     id: int
+#     name: str
+#     created_at: datetime
 
 
 @flow
 async def flow_up_reminder():
     logger = get_run_logger()
-    activities = await get_activities()
-    logger.info(f"Activities: {activities}")
+    # activities = await get_activities()
+    logger.info(f"Follow up reminder run at: {datetime.now()}")
 
 
-@task
-async def get_activities():
-    q = """
-    SELECT * FROM activities
-    """
+# @task
+# async def get_activities():
+#     q = """
+#     SELECT * FROM activities
+#     """
 
-    return await sqlalchemy_query(q, sqlalchemy_credentials)
+#     return await sqlalchemy_query(q, sqlalchemy_credentials)
 
 
 if __name__ == "__main__":
